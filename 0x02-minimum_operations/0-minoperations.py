9cc65cd19ed930c953192517044d15da48462a08
Sannithebaba <mubaraksanniajiferuke@gmail.com>
2023-07-13 14:11:59 -0700

    minoperation

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""
+This module contains a method that calculates the fewest number of operations
+needed to result in exactly n H characters in the file.
+Prototype: `minOperations(n)`
+Returns: integer
+if n is impossible to achieve, returns 0
+"""
